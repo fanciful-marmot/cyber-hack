020d8629d6b1bd9335cc26bdcef7c870ec450886
fanciful-marmot <daniel.crookston@gmail.com>
2021-04-09 11:51:42 -0700

    Webpack, babel, react, etc... setup

---
+++ src/ui/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div>Hello World!</div>
+    </div>
+  );
+};
+
+export { App };
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
